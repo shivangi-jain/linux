14d141f1d8237e991cba10b2d0ce668544b2eb7c
Shivangi Jain <shivangi.jain@sjsu.edu>
2020-03-06 19:40:34 -0800

    Procbased MSR addition done in code

--- cmpe281/cmpe283-1.c
+++ cmpe281/cmpe283-1.c
@@ -12,6 +12,10 @@
  * See SDM volume 4, section 2.1
  */
 #define IA32_VMX_PINBASED_CTLS	0x481
+#define IA32_VMX_PROCBASED_CTLS 0x482
+#define IA32_VMX_PROCBASED_CTLS2 0x48B
+#define IA32_VMX_EXIT_CTLS 0x483
+#define IA32_VMX_ENTRY_CTLS 0x484
 
 /*
  * struct caapability_info
@@ -37,6 +41,35 @@ struct capability_info pinbased[5] =
 	{ 6, "Activate VMX Preemption Timer" },
 	{ 7, "Process Posted Interrupts" }
 };
+
+
+struct capability_info procbased[21] = 
+{
+	{ 2, "Interrupt Window Exiting" },
+	{ 3, "Use TSC of fsetting" },
+	{ 7, "HLT exiting" },
+	{ 9, "INVLPG exiting" },
+	{ 10, "MWAIT Exiting" },
+	{ 11, "RDPMC Exiting" },
+	{ 12, "RDTSC Exiting" },
+	{ 15, "CR3 Load Exiting" },
+	{ 16, "CR3 Store Exiting" },
+	{ 19, "CR8 Load Exiting" },
+	{ 20, "CR8 Store Exiting" },
+	{ 21, "Use TPR Shadow" },
+	{ 22, "NMI window Exiting" },
+	{ 23, "MOV-DR Exiting" },
+	{ 24, "Unconditional IO Exiting" },
+	{ 25, " Use IO Bitmaps" },
+	{ 27, "Monitor Trap Flag" },
+	{ 28, "Use MSR Bitmaps" },
+	{ 29, "MONITOR Exiting" },
+	{ 30, "PAUSE Exiting" },
+	{ 31, "Activate Secondary controls" }
+};
+
+
+
 
 /*
  * report_capability
@@ -85,6 +118,12 @@ detect_vmx_features(void)
 	pr_info("Pinbased Controls MSR: 0x%llx\n",
 		(uint64_t)(lo | (uint64_t)hi << 32));
 	report_capability(pinbased, 5, lo, hi);
+
+	/* Procbased controls */
+	rdmsr(IA32_VMX_PROCBASED_CTLS, lo, hi);
+	pr_info("Procbased Controls MSR: 0x%llx\n",
+		(uint64_t)(lo | (uint64_t)hi << 32));
+	report_capability(procbased, 21, lo, hi);
 }
 
 /*
